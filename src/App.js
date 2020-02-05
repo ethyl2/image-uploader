4b0cf22d9a570b2d483d000021c390eba95de4b6
Heather Nuffer <zzzuzz@gmail.com>
2020-02-04 17:38:26 -0700

    Added login form to upload images

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import woman1 from './images/woman1.jpg';
+import axios from 'axios';
 
 function App() {
+  const handleChange = event => {
+    const imgPreview = document.getElementById('imgPreview');
+    console.log(event.target.files[0]);
+    const file = event.target.files[0];
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET)
+
+    axios({
+      url: process.env.REACT_APP_CLOUDINARY_URL,
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      data: formData
+  })
+      .then(res => {
+          console.log(res);
+          imgPreview.src = res.data.secure_url;
+
+      })
+      .catch(err => {
+          console.log(err);
+      })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Incredible Image Uploader</h1>
       </header>
+      <div className='main-content'>
+        <form>
+          <legend>Pick your image to upload:</legend>
+          <label htmlFor='imgSelection'>Select your file:</label>
+          <input type='file'
+            name='imgSelection'
+            id='imgSelection'
+            onChange={handleChange}
+          />
+        </form>
+        <div className='img-container'>
+          <img src={woman1} alt='uploaded placeholder' id='imgPreview' />
+        </div>
+      </div>
     </div>
   );
 }
